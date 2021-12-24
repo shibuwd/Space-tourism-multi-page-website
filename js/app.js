0517073817a52ee2d8108fa1569b6f947e72f7d6
shibuwd <shibusarkar619@gmail.com>
2021-12-24 22:18:28 +0600

    remove unuse code

--- js/app.js
+++ js/app.js
@@ -12,15 +12,6 @@ hamburger.addEventListener('click', () => {
 	mobile_menu.classList.toggle('active');
 });
 
-document.addEventListener('scroll', () => {
-	var scroll_position = window.scrollY;
-	if (scroll_position > 250) {
-		header.style.backgroundColor = '#fff';
-	} else {
-		header.style.backgroundColor = 'transparent';
-	}
-});
-
 menu_item.forEach(item => {
 	item.addEventListener('click', () => {
 		hamburger.classList.toggle('active');
